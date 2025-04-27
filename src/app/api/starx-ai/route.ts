ae8af092381bbf026a02f3118e870d828c51fe3e
fauzyDev <haliffauzy45@gmail.com>
2025-04-27 14:20:10 +0700

    fix

--- src/app/api/starx-ai/route.ts
+++ src/app/api/starx-ai/route.ts
@@ -1,7 +1,12 @@
-import { NextResponse } from 'next/server'
+import { NextResponse, NextRequest } from 'next/server'
 import { getCompeletion } from '@/libs/openai/api-call';
 
-export async function POST( message: string ) {
+export async function POST(req: NextRequest) {
+  try {
+    const { message } = await req.json();
     const content = await getCompeletion(message)
     return NextResponse.json({ status: 200, data: content, message: "Success" })
+  } catch (error) {
+    return NextResponse.json({ status: 500, message: "Internal Server Error" });
   }
+}
