a05ed1c28bd2fb452d6128a0713f503b9c156a07
fauzyDev <haliffauzy45@gmail.com>
2025-05-15 18:26:14 +0700

    add: code json view

--- src/components/Project/ProjectCard.jsx
+++ src/components/Project/ProjectCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Project from './Project';
+import response from "data/response.json"
 import project1 from "@/assets/images/project1.webp";
 import project2 from "@/assets/images/project2.webp";
 import project3 from "@/assets/images/project3.webp";
@@ -7,28 +8,39 @@ import project4 from "@/assets/images/cooming.webp";
 
 const project = [
   {
+    type: "image",
     imgSrc: project2,
     title: 'Spotify Clone',
     tags: ['API', 'MVC', 'Development'],
     projectLink: 'https://github.com/fauzyDev/Spotify-Clone'
   },
   {
+    type: "image",
     imgSrc: project1,
     title: 'Fananime list',
     tags: ['API', 'SPA'],
     projectLink: 'https://github.com/fauzyDev/Fananime-List'
   },
   {
+    type: "image",
     imgSrc: project3,
     title: 'Miko Ai | Chat ai',
     tags: ['Development', 'API'],
     projectLink: 'https://github.com/fauzyDev/MIKOCHAT-AI'
   },
   {
+    type: "image",
     imgSrc: project4,
     title: 'Album foto & video online',
     tags: ['Development', 'API'],
     projectLink: 'https://github.com/fauzyDev/Frontend-Web-album'
+  },
+  {
+    type: "json",
+    title: 'Cek Ongkir API',
+    tags: ['Express.js', 'API'],
+    jsonResponse: response,
+    projectLink: 'https://github.com/fauzyDev/cek-ongkir'
   },
 ];
 
@@ -41,9 +53,9 @@ const ProjectCard = () => {
         </h2>
         <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">
           {project.map(({
-            imgSrc, title, tags, projectLink
+            type, jsonResponse, imgSrc, title, tags, projectLink
           }, key) => (
-            <Project key={key} imgSrc={imgSrc} title={title} tags={tags} projectLink={projectLink} classes="reveal-up" />
+            <Project key={key} type={type} jsonResponse={jsonResponse} imgSrc={imgSrc} title={title} tags={tags} projectLink={projectLink} classes="reveal-up" />
           ))}
         </div>
       </div>
